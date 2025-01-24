7b0ce45f3163d81a05fee318c51ee6d1c648f7b6
Thibault Meunier <thibault@cloudflare.com>
2025-01-24 18:01:31 +0100

    Add implementation for arbitrary batched response

--- src/arbitrary_batched_token.ts
+++ src/arbitrary_batched_token.ts
@@ -1,6 +1,12 @@
 import { TOKEN_TYPES, tokenRequestToTokenTypeEntry } from './index.js';
-import { TokenRequest as Type1TokenRequest } from './priv_verif_token.js';
-import { TokenRequest as Type2TokenRequest } from './pub_verif_token.js';
+import {
+    TokenRequest as Type1TokenRequest,
+    TokenResponse as Type1TokenResponse,
+} from './priv_verif_token.js';
+import {
+    TokenRequest as Type2TokenRequest,
+    TokenResponse as Type2TokenResponse,
+} from './pub_verif_token.js';
 import { joinAll } from './util';
 
 export class TokenRequest {
@@ -58,10 +64,10 @@ export class BatchedTokenRequest {
         while (offset < bytes.length) {
             const len = input.getUint16(offset);
             offset += 2;
-            const blindedMsg = new Uint8Array(input.buffer.slice(offset, offset + len));
+            const b = new Uint8Array(input.buffer.slice(offset, offset + len));
             offset += len;
 
-            batchedTokenRequests.push(TokenRequest.deserialize(blindedMsg));
+            batchedTokenRequests.push(TokenRequest.deserialize(b));
         }
 
         return new BatchedTokenRequest(batchedTokenRequests);
@@ -81,6 +87,84 @@ export class BatchedTokenRequest {
 
             output.push(tokenRequestSerialized);
             length += tokenRequestSerialized.length;
+        }
+
+        const b = new ArrayBuffer(2);
+        new DataView(b).setUint16(0, length);
+
+        return new Uint8Array(joinAll([b, ...output]));
+    }
+}
+
+export class OptionalTokenResponse {
+    // struct {
+    //     TokenResponse token_response<0..2^16-1>; /* Defined by token_type */
+    // } OptionalTokenResponse;
+    constructor(public readonly tokenResponse: null | Uint8Array) {}
+
+    static deserialize(bytes: Uint8Array): OptionalTokenResponse {
+        if (bytes.length === 0) {
+            return new OptionalTokenResponse(null);
+        }
+        return new OptionalTokenResponse(bytes);
+    }
+
+    serialize(): Uint8Array {
+        if (this.tokenResponse === null) {
+            return new Uint8Array();
+        }
+        return this.tokenResponse;
+    }
+}
+
+// struct {
+//     OptionalTokenResponse token_responses<0..2^16-1>;
+// } BatchTokenResponse
+export class BatchedTokenResponse {
+    // struct {
+    //     TokenRequest token_requests<0..2^16-1>;
+    // } BatchTokenRequest
+    constructor(public readonly tokenResponses: OptionalTokenResponse[]) {}
+
+    static deserialize(bytes: Uint8Array): BatchedTokenResponse {
+        let offset = 0;
+        const input = new DataView(bytes.buffer);
+
+        const length = input.getUint16(offset);
+        offset += 2;
+
+        if (length != bytes.length + offset) {
+            throw new Error('provided bytes does not match its encoded length');
+        }
+
+        const batchedTokenResponses: OptionalTokenResponse[] = [];
+
+        while (offset < bytes.length) {
+            const len = input.getUint16(offset);
+            offset += 2;
+            const b = new Uint8Array(input.buffer.slice(offset, offset + len));
+            offset += len;
+
+            batchedTokenResponses.push(OptionalTokenResponse.deserialize(b));
+        }
+
+        return new BatchedTokenResponse(batchedTokenResponses);
+    }
+
+    serialize(): Uint8Array {
+        const output = new Array<ArrayBuffer>();
+
+        let length = 0;
+        for (const tokenResponse of this.tokenResponses) {
+            const tokenResponseSerialized = tokenResponse.serialize();
+
+            const b = new ArrayBuffer(2);
+            new DataView(b).setUint16(0, tokenResponseSerialized.length);
+            output.push(b);
+            length += 2;
+
+            output.push(tokenResponseSerialized);
+            length += tokenResponseSerialized.length;
         }
 
         const b = new ArrayBuffer(2);
